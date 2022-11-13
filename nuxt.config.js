d9a3b7a7bbe477fbf4a76e4d6c973dd61b9f9246
BeibarysMaksatuly <beibarys.m.03@gmail.com>
2022-11-14 00:09:01 +0600

    fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: 'aviata-test',
         htmlAttrs: {
@@ -14,23 +13,15 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
         "@/assets/css/main.scss",
         "@/assets/css/custom.scss",
     ],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/vuetify
         '@nuxtjs/vuetify',
     ],
-
-    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
     vuetify: {
         theme: {
             dark: false,
